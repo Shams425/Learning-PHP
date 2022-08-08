8f309ad737d8fb74b9ba746975560701a1493f55
Shams425 <alexdavid486@gmail.com>
2022-08-08 22:42:24 +0200

    learning the predefine variable Get

--- Form.php
+++ Form.php
@@ -16,8 +16,22 @@
   </form>
   <?php
     if(isset($_POST['firstName']) && $_POST['lastName']) {
-      echo $_POST['firstName'];
-      echo $_POST['lastName'];
+      echo "<h2>" . $_POST['firstName'] . "</h2>";
+      echo "<h2>" . $_POST['lastName'] . "</h2>";
+    }
+  ?>
+
+  <form action="#" Method="GET">
+    <label for="text">Email</label>
+    <input type="mail" name="e-mail">
+    <label for="text">Phone Number</label>
+    <input type="number" name="phoneNum">
+  </form>
+
+  <?php
+    if(isset($_GET['firstName']) && $_GET['lastName']) {
+      echo "<h2>" . $_GET['e-mail'] . "</h2>";
+      echo "<h2>" . $_GET['phoneNum'] . "</h2>";
     }
   ?>
 </body>
